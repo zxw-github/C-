b0a6b210d299be8e8e05c61bb4b4bfc1d27325e5
huaian-code <2056757071@qq.com>
2023-02-28 20:25:01 +0800

    2月28日

---
+++ 10.内存函数/10.1内存拷贝_memcpy.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+// memcpy -- 内存拷贝
+/*
+#include <string.h>
+void *memcpy( void *to, const void *from, size_t count );
+函数从from中复制count个字节到to中，并返回to指针。 如果to 和 from 重叠，则函数行为不确定
+*/
+void test_memcpy();
+void test_my_memcpy();
+void my_memcpy(void *to, const void *from, size_t num);
+void print(int arr[], int sz);
+int main(void)
+{
+    // test_memcpy();
+    test_my_memcpy();
+    return 0;
+}
+void test_memcpy()
+{
+    int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int arr2[10] = {0};
+
+    memcpy(arr2, arr1, 20); // 按字节为单位拷贝
+}
+void test_my_memcpy()
+{
+    int arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int arr2[10] = {0};
+
+    my_memcpy(arr2, arr1, 20);
+    print(arr2, 10);
+}
+void my_memcpy(void *to, const void *from, size_t num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        *(char *)to = *(char *)from;
+        to = (char *)to + 1;
+        from = (char *)from + 1;
+    }
+}
+void print(int arr[], int sz)
+{
+    for (int i = 0; i < sz; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
